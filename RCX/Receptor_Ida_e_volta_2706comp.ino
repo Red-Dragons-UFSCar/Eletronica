393c18a6a12b0628d25b555a8f871ce1928a21a1
sealsrar <57424474+sealsrar@users.noreply.github.com>
2023-07-27 13:27:55 -0300

    🐊🐊

--- RCX/Receptor_Ida_e_volta_2706comp.ino
+++ RCX/Receptor_Ida_e_volta_2706comp.ino
@@ -1,12 +1,17 @@
+// BIBLIOTECAS
+//#include <esp_now.h>
+//#include <WiFi.h>
+
 hw_timer_t* timer = NULL;
 #define ROBOT 3
+#define WIFI_CHANNEL 12 
 
 // Bibliotecas para Comunicação ESP-NOW
-#define WIFI_CHANNEL 14
+
+
 #include <esp_now.h>
 #include <WiFi.h>
 #include <esp_wifi.h>
-
 
 // Endereço de broadcast do Transmissor
 uint8_t broadcastAddress[] = {0X40, 0x22, 0xD8, 0x03, 0xCE, 0xF0};
@@ -20,7 +25,8 @@ typedef struct struct_message {
   // Tamanho do vetor utilizado para receber a mensagem
   const static int lengthVector = 10;
   //  Vetor da mensagem
-  uint8_t RD[lengthVector] = {111, 170, 160, 0, 0, 0, 0, 50, 50, 113};
+  uint8_t RD[lengthVector] = {111, 0, 0, 0, 0, 0, 0, 0, 0, 112};
+  int cont = 0;
 } struct_message;
 
 typedef struct return_message {
@@ -40,10 +46,10 @@ return_message ReturnData;
 int ledState = HIGH;
 
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  
+  /*
   Serial.print("\r\nLast Packet Send Status:\t");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-  
+  */
 }
 
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
@@ -93,47 +99,37 @@ volatile int  counterAB = 0;
 volatile int  counterAB_b = 0;
 double leitura = 0;
 
+//Variaveis de calculos de velocidade
+volatile double vel = 0;
+volatile double vel_b = 0;
+
 
 // CONTROLE
-volatile int ErroAnterior1A = 0;
-volatile int ErroAnterior1B = 0;
-int ErroAtualA = 0;
-int ErroAtualB = 0;
-volatile int Escala_P1 = 0;
-volatile int Escala_I1 = 0;
-volatile int Escala_P2 = 0;
-volatile int Escala_I2 = 0;
+volatile int erro_a = 0;
+volatile int erro_b = 0;
+float somaerro_a = 0;
+float somaerro_b = 0;
+volatile double ap_a = 0;
+volatile double ap_b = 0;
+volatile double kp_a = 4;
+volatile double ki_a = 0.04;
+volatile int Escala_P = 1;
+volatile int Escala_I = 1;
 volatile int ref_a = 0;
-volatile int ref_b = 0;
-int SaidaA = 0;
-int SaidaB =0;
+volatile int  ref_b = 0;
+volatile double ac_a = 0;
+volatile double ac_b = 0;
+volatile double eintegral_a = 0;
+volatile double eintegral_b = 0;
 int pwm_a;
 int pwm_b;
-volatile double KpA = 1.5;
-volatile int KiA = 50;
-volatile double KpB = 50;
-volatile int KiB = 1.7;
-volatile double Soma_A = KpA+(KiA/100);
-volatile double Soma_B = KpB+(KiB/100);
-volatile int SaidaAnteriorA = 0;
-volatile int SaidaAnteriorB = 0;
-volatile double AnteriorB = 0;
-volatile double Anterior2B = 0;
-volatile double AnteriorA = 0;
-volatile double Anterior2A = 0;
-int contA = 1;
-int contB = 1;
-volatile double compA;
-volatile double compB;
-volatile double valA;
-volatile double valB;
-int stateA = 0;
-int stateB = 0;
-int teste = 0;
+
+
 //SOMADOR ENCODERS
 void IRAM_ATTR ai1() {
   // Verificar possibilidade de retirar o if desta função
   counterAB ++;
+  
 }
 
 void IRAM_ATTR ai2() {
@@ -159,6 +155,12 @@ void IRAM_ATTR onTime()
   if (myData.dataAvaliable) {
     if(myData.RD[0] == 111){
       if (ROBOT == 1) {
+        if(ref_a != (myData.RD[3])){
+          eintegral_a = 0;
+        }
+        if (ref_b != (myData.RD[4])){
+          eintegral_b = 0;
+        }
         ref_a = myData.RD[3];
         ref_b = myData.RD[4];
         direcao1[0] = (*(myData.RD + 1) >> 7) & 1;
@@ -166,6 +168,12 @@ void IRAM_ATTR onTime()
         direcao2[0] = (*(myData.RD + 1) >> 5) & 1;
         direcao2[1] = (*(myData.RD + 1) >> 4) & 1;
       } else if (ROBOT == 2) {
+        if(ref_a != myData.RD[5]){
+          eintegral_a = 0;
+        }
+        if(ref_b != myData.RD[6]){
+          eintegral_b =0;
+        }
         ref_a = myData.RD[5];
         ref_b = myData.RD[6];
         direcao1[0] = (*(myData.RD + 1) >> 3) & 1;
@@ -173,6 +181,12 @@ void IRAM_ATTR onTime()
         direcao2[0] = (*(myData.RD + 1) >> 1) & 1;
         direcao2[1] = (*(myData.RD + 1) >> 0) & 1;
       } else if (ROBOT == 3) {
+        if (ref_a != myData.RD[7]){
+          eintegral_a =0;
+        }
+        if(ref_b != myData.RD[8]){
+          eintegral_b = 0;
+        }
         ref_a = myData.RD[7];
         ref_b = myData.RD[8];
         direcao1[0] = (*(myData.RD + 2) >> 7) & 1;
@@ -182,86 +196,73 @@ void IRAM_ATTR onTime()
       }
 
     
+    else {
+      ref_a = 0;
+      ref_b = 0;
+      direcao1[0] = false;
+      direcao1[1] = false;
+      direcao2[0] = false;
+      direcao2[1] = false;
+    }
 
-    //Erro 
-    ErroAtualA = ref_a - counterAB; // PQ Q O COUNTER TA INDO ATE 15 só ??????????????????????
-    
+    // Calculo de velocidade da roda A
+    vel = counterAB * 1.7409;
+    //Serial.print(counterAB);
     counterAB = 0;
-    ErroAtualA = min(ErroAtualA, 30);
 
-    ErroAtualB = ref_b - counterAB_b;
+    // Calculo de velocidade da roda B
+    vel_b = counterAB_b * 1.7409;
+
     counterAB_b = 0;
-    ErroAtualB = min(ErroAtualB, 30);
 
-    compA=ErroAtualA + ErroAnterior1A;
-    
-    if(compA > 50){
-      SaidaA = SaidaAnteriorA;
-    }
-    else{
-      AnteriorA = (KpA*ErroAnterior1A);
-      Anterior2A = (ErroAtualA*Soma_A);
-      //SaidaA = Anterior2A - AnteriorA + SaidaAnteriorA;
-      SaidaA = Anterior2A - AnteriorA + SaidaAnteriorA;
+    // Inicio controle
+    // calculo dos erros
+    erro_a = (ref_a - vel);
+    erro_b = (ref_b - vel_b);
 
-    }
+    //calculo das ações proporcionais
+    ap_a = erro_a * kp_a;
+    ap_b = erro_b * kp_a;
 
-    valA=contA*23;
-    if((SaidaA >= valA)){
-    SaidaA = valA;
-    contA++;
-    }
-    else{
-    contA = 1;
-    }
-    
-    compB=ErroAtualB + ErroAnterior1B;
- 
-    if(compB > 50){
-      SaidaB = SaidaAnteriorB;
-    }
-    else{
-      AnteriorB = (KpB*ErroAnterior1B);
-      Anterior2B = (ErroAtualB*Soma_B);
-      SaidaB = Anterior2B - AnteriorB + SaidaAnteriorB;;
-    }
-       valB=contB*23;
-       if(SaidaB >= valB){
-       SaidaB = valB;
-       contB++;
-       }
-       else {
-       contB = 1;
-       }
-    ErroAnterior1A = ErroAtualA;
-    ErroAnterior1B = ErroAtualB;
-    SaidaAnteriorA = SaidaA;
-    SaidaAnteriorB = SaidaB;
-    
-    pwm_b = map(SaidaB,0,30,150,255);
-    pwm_a = map(SaidaA,0,30,150,255);
-    pwm_a = max(pwm_a,0);
-    pwm_a = min(pwm_a,255);
-    pwm_b = max(pwm_b,0);
-    pwm_b = min(pwm_b,255);
-    motor(pwm_a,pwm_b);
-    
+    //Calculo das ações integrativas
+    eintegral_a = eintegral_a + erro_a * 0.01;
+    eintegral_b = eintegral_b + erro_b * 0.01;
+  
+
+    // Mapeamento do erro
+    ac_a = map(ap_a + eintegral_a * ki_a, -90, 90, -255, 255);
+    ac_b = map(ap_b + eintegral_b * ki_a, -90, 90, -255, 255);
+
+    // Correção do dutycycle após operações com o erro
+    pwm_a = dutyCycle + ac_a;
+    pwm_b = dutyCycle_b + ac_b;
+
+    pwm_a = max(pwm_a, 0);
+    pwm_a = min(pwm_a, 255);
+    pwm_b = max(pwm_b, 0);
+    pwm_b = min(pwm_b, 255);
+
+    dutyCycle = pwm_a;
+    dutyCycle_b = pwm_b;
+    motor(dutyCycle,dutyCycle_b);
     } else if (myData.RD[0]== 112){
-      Escala_P1 = myData.RD[5];
-      Escala_I1 = myData.RD[6];
-      Escala_P2 = myData.RD[7];
-      Escala_P2 = myData.RD[8];
-      KpA = myData.RD[1]*(pow(10,Escala_P1*(-1)));
-      KiA = myData.RD[2]*(pow(10,Escala_I1*(-1)));
-      KpB = myData.RD[3]*(pow(10,Escala_P2*(-1)));
-      KiB = myData.RD[4]*(pow(10,Escala_I2*(-1)));
-      Soma_A = KpA+(KiA/100);
-      Soma_B = KpB+(KiB/100);
+      Escala_P = myData.RD[1];
+      Escala_I = myData.RD[2];
+      if(ROBOT == 1){
+          kp_a = myData.RD[3]*(pow(10,Escala_P*(-1)));
+          ki_a = myData.RD[4]*(pow(10,Escala_P*(-1)));
+      } else if (ROBOT ==2){
+          kp_a = myData.RD[5]*(pow(10,Escala_P*(-1)));
+          ki_a = myData.RD[6]*(pow(10,Escala_P*(-1)));
+      } else if(ROBOT ==3){
+          kp_a = myData.RD[7]*(pow(10,Escala_P*(-1)));
+          ki_a = myData.RD[8]*(pow(10,Escala_P*(-1)));
+
+      }
     }
   }
-  
+  contador++;
 }
-
 
 
 double ReadVoltage(byte pin){
@@ -306,7 +307,7 @@ void setup() {
   timerAttachInterrupt(timer, &onTime, true);
   timerAlarmWrite(timer,10000,true);
   timerAlarmEnable(timer);
-  
+
   ESP_ERROR_CHECK(esp_netif_init());
   ESP_ERROR_CHECK(esp_event_loop_create_default());
   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
@@ -315,7 +316,7 @@ void setup() {
   ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
   ESP_ERROR_CHECK( esp_wifi_start());
   ESP_ERROR_CHECK( esp_wifi_set_channel(WIFI_CHANNEL, WIFI_SECOND_CHAN_NONE));
-  
+
 
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
@@ -334,10 +335,16 @@ void setup() {
   
   
   esp_now_register_send_cb(OnDataSent);
-  
+
 }
+
+
+
+
 //Função para obtenção de informações da comunicação, correção de erro e controle final dos motores.
 //AVISO !! CASO SEJA NECESSARIO PRINTAR ALGUMA VARIÁVEL DESTE CAMPO CASO ELE SEJA USADO EM INTERRUPÇÃO UTILIZE O VOID LOOP
+
+
 void loop()
 {
   leitura = ReadVoltage(READ)*32;
@@ -346,15 +353,9 @@ void loop()
       digitalWrite(BAT_LED,HIGH);
     }
     ReturnData.RD_R = leitura;
-    //Serial.println(leitura);
+    Serial.println(leitura);
     esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &ReturnData, sizeof(ReturnData));
     contador = 0;
-   
   }
-  if(teste)
-  {
-    Serial.println(counterAB);
-    teste = 0;
-  }
-  
+
 }
